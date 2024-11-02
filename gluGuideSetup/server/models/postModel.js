5f9d1eedd2f1a3c1c7503d2557e5311eddbaf8b9
Emili Sommer <eliza.liebt.sommer@gmail.com>
2024-11-02 22:54:29 +0100

    postModel

---
+++ gluGuideSetup/server/models/postModel.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./index');
+
+const Post = sequelize.define('Post', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+module.exports = Post;

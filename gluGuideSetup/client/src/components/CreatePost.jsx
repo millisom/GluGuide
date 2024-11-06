bcd4290cb1b5808d2ffd2072e2d07ba9aa731d74
Emili Sommer <eliza.liebt.sommer@gmail.com>
2024-11-06 09:19:53 +0100

    Frontend Upload picture in 'Create Post'

--- gluGuideSetup/client/src/components/CreatePost.jsx
+++ gluGuideSetup/client/src/components/CreatePost.jsx
@@ -6,18 +6,30 @@ import 'react-quill/dist/quill.snow.css'; // Import Quill's CSS
 const CreatePost = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [postPicture, setPostPicture] = useState(null);
   const [error, setError] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
 
+
+  const handleFileChange = (e) => {
+    setPostPicture(e.target.files[0]);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('content', content);
+    if (postPicture) formData.append('postPicture', postPicture);
     try {
         // Make the POST request
-        const response = await axios.post('http://localhost:8080/CreatePost', { title, content }, {withCredentials: true});
+        const response = await axios.post('http://localhost:8080/CreatePost', formData, {withCredentials: true});
+        headers: { 'Content-Type': 'multipart/form-data' },
         console.log('Post created:', response.data); // Log the response for debugging
         // Reset fields or handle success here
         setTitle('');
         setContent('');
+        setPostPicture(null);
         setSuccessMessage('Post created successfully!');
       } catch (error) {
         if (error.response && error.response.status === 401) {
@@ -51,6 +63,15 @@ const CreatePost = () => {
             required
           />
         </div>
+        <div>
+          <label>Upload Picture:</label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </div>
+
         <button type="submit">Create Post</button>
         {error && <p>{error}</p>} {/* Display error message */}
         {successMessage && <p>{successMessage}</p>} {/* Display success message */}

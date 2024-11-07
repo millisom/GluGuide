c7bcd9b35dca7aa23b3bdf3e65bb7b832a8e0d45
hossei-cyber <hussayaqobzai@gmail.com>
2024-11-07 02:04:22 +0100

    .

--- gluGuideSetup/client/src/pages/homepage.jsx
+++ gluGuideSetup/client/src/pages/homepage.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import ResetPasswordForm from "../components/ResetPasswordForm";
-import ForgotPasswordForm from '../components/ForgotPasswordForm';
 const Homepage = () => {
 
     return (
         <div className="page-container">
-            <ResetPasswordForm />
         <h1>Welcome to GluGuide!</h1>
-
         <p>
             GluGuide is a platform that helps you keep track of your blood sugar
             levels and provides you with personalized recommendations to help you
             manage your diabetes.
         </p>
-        <p>
-            To get started, please sign up or log in.
-        </p>
-        <ForgotPasswordForm />
         </div>
     );
 }

e10f8e497dedbde0774eab0b3dc2df8fd11091ff
Maja Jurkowska <maja.jurkowska@mecodia.de>
2025-05-11 22:11:22 +0200

    Refactor app.jsx

--- gluGuideSetup/client/src/App.jsx
+++ gluGuideSetup/client/src/App.jsx
@@ -1,69 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar.jsx';
-import HomePage from './pages/homepage.jsx';
-import MyAccount from './pages/myAccount.jsx';
-import ContactUs from './pages/contactUs.jsx';
-import AboutUs from './pages/aboutUs.jsx';
-import LoginPage from './pages/login.jsx';
-import SignUp from './pages/signUp.jsx';
-import Blogs from './pages/blogs.jsx';
-import ViewPost from './components/viewPost.jsx';   
-import EditPost from './components/editPost.jsx';  
-import CreatePost from './pages/createPost.jsx';
-import MyBlogs from './pages/myBlogs.jsx';
-import ForgotPassword from './pages/forgotPassword.jsx';
-import ResetPassword from './pages/resetPassword.jsx';
-import AdminDashboard from './pages/AdminDashboard';
-import AdminCreateUser from './pages/AdminCreateUser';
-import AdminEditUser from './pages/AdminEditUser';
-import Footer from './components/Footer';
-import AdminEditPost from './pages/AdminEditPost';
-import UserProfile from './components/UserProfile.jsx'; // Import the new component
-import LogMealPage from './pages/LogMealPage.jsx';
-import CreateRecipePage from './pages/CreateRecipePage.jsx';
-import SummaryPage from './pages/RecipeSummaryPage.jsx';
-import RecipeCardRoute from './components/RecipeCardRoute.jsx';
-import MealsOverviewPage from './pages/MealsOverviewPage.jsx'; // Import the new component
-import MealCardRoute from './components/MealCardRoute.jsx';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppLayout from './components/layout/AppLayout';
+import AppRoutes from './routes';
 
 function App() {
-    return (
-        <Router>
-            <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-                <Navbar />
-                <main style={{ flex: 1 }}>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/account" element={<MyAccount />} />
-                        <Route path="/contact" element={<ContactUs />} />
-                        <Route path="/about" element={<AboutUs />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/signUp" element={<SignUp />} />
-                        <Route path="/blogs" element={<Blogs />} />
-                        <Route path="/blogs/view/:id" element={<ViewPost />} />
-                        <Route path="/blogs/edit/:id" element={<EditPost />} />
-                        <Route path="/create/post" element={<CreatePost />} />
-                        <Route path="/myBlogs" element={<MyBlogs />} />
-                        <Route path="/viewPost/:id" element={<ViewPost />} />
-                        <Route path="/profile/:username" element={<UserProfile />} /> {/* Add this route */}
-                        <Route path="/forgotPassword" element={<ForgotPassword />} />
-                        <Route path="/resetPassword/:token" element={<ResetPassword />} />
-                        <Route path="/admin" element={<AdminDashboard />} />
-                        <Route path="/admin/createUser" element={<AdminCreateUser />} />
-                        <Route path="/admin/editUser/:id" element={<AdminEditUser />} />
-                        <Route path="/admin/editPost/:id" element={<AdminEditPost />} />
-                        <Route path="/logMeal" element={<LogMealPage />} />
-                        <Route path="/createRecipe" element={<CreateRecipePage />} />
-                        <Route path="/Recipes" element={<SummaryPage />} />
-                        <Route path="/recipes/:id" element={<RecipeCardRoute />} />
-                        <Route path="/mealsOverview" element={<MealsOverviewPage />} /> {/* Add this route */}
-                        <Route path="/meals/:mealId" element={<MealCardRoute />} /> {/* Add this route */}
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <AppLayout>
+        <AppRoutes />
+      </AppLayout>
+    </Router>
+  );
 }
 
 export default App;

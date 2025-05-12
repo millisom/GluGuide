f7097d3992ca7f27e525e4cd7afc98eec64786cd
Emili Sommer <eliza.liebt.sommer@gmail.com>
2025-05-12 19:08:57 +0200

    shifted css file to styles

--- gluGuideSetup/client/src/pages/signUp.jsx
+++ gluGuideSetup/client/src/pages/signUp.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import './signup.css';
+import '../styles/signUp.module.css';
+
 function SignUp() {
     const history = useNavigate();
     const [username, setUsername] = useState('');

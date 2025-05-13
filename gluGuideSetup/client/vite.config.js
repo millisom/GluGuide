f5710ad190f9142f4cf92e4125713679b0b045d5
Emili Sommer <eliza.liebt.sommer@gmail.com>
2025-05-14 01:34:22 +0200

    added variables based on mode for deploying or hosting

--- gluGuideSetup/client/vite.config.js
+++ gluGuideSetup/client/vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    include: ['**/*.test.js', '**/*.test.jsx'],
-    exclude: ['node_modules'],
-  },
+export default defineConfig(({ mode }) => {
+  // Load env variables based on mode (.env.production, .env.local, etc.)
+  const env = loadEnv(mode, process.cwd(), '')
 
-  server: {
-    hmr: {
+  return {
+    plugins: [react()],
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      include: ['**/*.test.js', '**/*.test.jsx'],
+      exclude: ['node_modules'],
+    },
+    server: {
+      hmr: {
         overlay: false
+      }
     }
-}
+  }
 })

2ad2f72a3fb659979e369a7f8f2342a3a23d990a
Emili Sommer <eliza.liebt.sommer@gmail.com>
2025-05-16 16:52:25 +0200

    add standard proxy

--- gluGuideSetup/server/config/sessionConfig.js
+++ gluGuideSetup/server/config/sessionConfig.js
@@ -2,17 +2,20 @@ const session = require('express-session');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
+app.set('trust proxy', 1);
+
+
 const sessionConfig = (app) => {
   app.use(session({
-    name: 'gluguide.sid', // Optional, but helps identify the session
+    name: 'gluguide.sid', 
     secret: process.env.SESSION_SECRET || 'default_secret',
     saveUninitialized: false,
     resave: false,
     cookie: {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60, // 1 hour
-      secure: isProduction,  // ✅ HTTPS only in production
-      sameSite: isProduction ? 'none' : 'lax', // ✅ allow cross-site cookies in prod
+      maxAge: 1000 * 60 * 60, 
+      secure: isProduction,  
+      sameSite: isProduction ? 'none' : 'lax', 
     },
   }));
 };

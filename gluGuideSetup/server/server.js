a4637102dceaa0a6bea0184bcb1e5d96be66b36f
Emili Sommer <eliza.liebt.sommer@gmail.com>
2025-05-16 17:45:34 +0200

    fixing Port locally

--- gluGuideSetup/server/server.js
+++ gluGuideSetup/server/server.js
@@ -30,7 +30,7 @@ setupMiddleware(app);
 setupRoutes(app);
 
 // Start the server
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log('Server is running on http://localhost:8080');
 });
